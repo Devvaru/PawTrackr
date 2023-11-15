1e83041f4e0d2c920fb29410e91d13cfeaab75a8
Devvaru <syeraru@gmail.com>
2023-11-14 17:47:42 -0700

    Add dotenv to connection.js

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const dbParams = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 };
 
 const db = new Pool(dbParams);
