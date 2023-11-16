87ba33222715688212aad7c9e74d7b6ba43a6795
Devvaru <syeraru@gmail.com>
2023-11-16 12:42:55 -0700

    Made accordion

---
+++ frontend/src/components/FormAccordion.jsx
@@ -0,0 +1,46 @@
+import { useContext } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+import AccordionContext from 'react-bootstrap/AccordionContext';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import Card from 'react-bootstrap/Card';
+
+const PINK = 'rgba(255, 192, 203, 0.6)';
+const BLUE = 'rgba(0, 0, 255, 0.6)';
+
+function ContextAwareToggle({ children, eventKey, callback }) {
+  const { activeEventKey } = useContext(AccordionContext);
+
+  const decoratedOnClick = useAccordionButton(
+    eventKey,
+    () => callback && callback(eventKey),
+  );
+
+  const isCurrentEventKey = activeEventKey === eventKey;
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: isCurrentEventKey ? PINK : BLUE }}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function FormAccordion() {
+  return (
+    <Accordion defaultActiveKey="0">
+      <Card>
+        <Card.Header>
+          <ContextAwareToggle eventKey="0">Click me!</ContextAwareToggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I am the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+}
+
+export default FormAccordion;
