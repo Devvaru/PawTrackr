f85b3728aeb5f619b721efec0957cf06b77e4e0e
Devvaru <syeraru@gmail.com>
2023-11-16 14:43:31 -0700

    Create reminders table and populate

---
+++ frontend/src/components/ReminderItem.jsx
@@ -0,0 +1,22 @@
+function ReminderItem(props) {
+  const { reminder } = props;
+
+  // Display Reminder Date in YYYY/MM/DD format
+  const reminderDate = new Date(reminder.date);
+  const formattedReminderDate = `${reminderDate.getFullYear()}/${(
+    reminderDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, '0')}/${reminderDate.getDate().toString().padStart(2, '0')} ${reminderDate.getHours().toString().padStart(2, '0')}:${reminderDate.getMinutes().toString().padStart(2, '0')}`;
+
+
+  return (
+    <>
+      <td>{reminder.title}</td>
+      <td>{'---'}{formattedReminderDate}{'---'}</td>
+      <td>{reminder.comment}</td>
+    </>
+  )
+}
+
+export default ReminderItem;
---
+++ frontend/src/components/RemindersTable.jsx
@@ -0,0 +1,67 @@
+import Table from 'react-bootstrap/Table';
+import ReminderItem from './ReminderItem';
+import FormAccordion from './FormAccordion';
+import Row from 'react-bootstrap/Row';
+import { useState, useEffect } from 'react';
+
+function RemindersTable() {
+
+  const [reminders, setReminders] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/reminders')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setReminders(data);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Table responsive="sm">
+        <div>
+
+          {/* <FormAccordion /> */}
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Comment</th>
+            </tr>
+          </thead>
+          {reminders.length !== 0 && (
+            <tbody>
+
+              {
+                reminders.map((reminder) => (
+                  <tr>
+                    <ReminderItem
+                      key={reminder.id}
+                      reminder={reminder}
+                    />
+                  </tr>
+                ))
+              }
+
+            </tbody>
+
+
+          )}
+          {reminders.length === 0 && <div>Loading</div>}
+        </div>
+
+
+      </Table>
+    </div>
+  );
+}
+
+export default RemindersTable;
