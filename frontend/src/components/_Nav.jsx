bbd2d52e487ed39624bd81068f59998f3be98c95
Devvaru <syeraru@gmail.com>
2023-11-16 15:33:56 -0700

    Set up state for nav items

--- frontend/src/components/_Nav.jsx
+++ frontend/src/components/_Nav.jsx
@@ -1,17 +1,30 @@
+import { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function ColorSchemesExample() {
+function ColorSchemesExample(props) {
+  const { showPets, setShowPets, showReminders, setShowReminders } = props;
+
+  useEffect(() => {
+    if (showPets) {
+      setShowReminders(false);
+    } 
+
+    if (showReminders) {
+      setShowPets(false);
+    }
+  }, [showPets, showReminders]);
+
   return (
     <>
       <Navbar bg='dark' data-bs-theme='dark'>
         <Container>
-          <Navbar.Brand href='#home'>PawTrackr</Navbar.Brand>
+          <Navbar.Brand onClick={() => setShowPets(true)}>PawTrackr</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link href='#home'>Pets</Nav.Link>
-            <Nav.Link href='#reminders'>Reminders</Nav.Link>
-            <Nav.Link href='#contacts'>Contacts</Nav.Link>
+            <Nav.Link onClick={() => setShowPets(true)}>Pets</Nav.Link>
+            <Nav.Link onClick={() => setShowReminders(true)}>Reminders</Nav.Link>
+            {/* <Nav.Link href='#contacts'>Contacts</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
