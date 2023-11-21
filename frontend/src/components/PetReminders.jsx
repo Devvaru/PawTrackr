64c2505c3013cdec0ae1c6d0948a097f56b0b911
Shivangi Srivastava <shi06dec@gmail.com>
2023-11-21 12:49:04 -0700

    Display fake reminders data

---
+++ frontend/src/components/PetReminders.jsx
@@ -0,0 +1,18 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function PetReminders() {
+
+  return (
+    <>
+      <ListGroup>
+        <ListGroup.Item><strong>Upcoming Reminders:</strong></ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+    </>
+  );
+}
+
+export default PetReminders;
