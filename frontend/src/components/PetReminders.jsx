15281678758b0ad1ef91fbdfb716ce21497b157d
Shivangi Srivastava <shi06dec@gmail.com>
2023-11-21 13:36:12 -0700

    Render pet reminders using axios

--- frontend/src/components/PetReminders.jsx
+++ frontend/src/components/PetReminders.jsx
@@ -1,15 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import axios from 'axios';
 
 function PetReminders(props) {
   const { pet } = props;
+  const [petReminders, setPetReminders] = useState([]);
+
+  // Display Reminder Date in YYYY/MM/DD format
+  // const reminderDate = new Date(completedReminder.date);
+  // const formattedReminderDate = `${reminderDate.getFullYear()}/${(
+  //   reminderDate.getMonth() + 1
+  // )
+  //   .toString()
+  //   .padStart(2, '0')}/${reminderDate
+  //     .getDate()
+  //     .toString()
+  //     .padStart(2, '0')} ${reminderDate
+  //       .getHours()
+  //       .toString()
+  //       .padStart(2, '0')}:${reminderDate
+  //         .getMinutes()
+  //         .toString()
+  //         .padStart(2, '0')}`;
+
 
   useEffect(() => {
     axios
       .get(`http://localhost:8080/api/reminders/${pet.id}`)
       .then((res) => {
         console.log(res);
+        setPetReminders(res.data)
       })
       .catch((err) => {
         console.log({ err });
@@ -20,10 +40,10 @@ function PetReminders(props) {
     <>
       <ListGroup>
         <ListGroup.Item><strong>Upcoming Reminders:</strong></ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        {
+          petReminders.map((petreminder) => (
+          <ListGroup.Item key={petreminder.id}>{petreminder.date} - {petreminder.title} -  {petreminder.comment}</ListGroup.Item>
+        ))}
     </ListGroup>
     </>
   );
