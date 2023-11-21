dbd7afa4da17a9bc57835a5458bd0de079c3d11c
kazvee <kayvee123@gmail.com>
2023-11-21 16:05:54 -0700

    Remove unneeded console logs

--- frontend/src/components/ContactAccordion.jsx
+++ frontend/src/components/ContactAccordion.jsx
@@ -6,7 +6,6 @@ import Button from 'react-bootstrap/Button';
 
 function CustomToggle({ children, eventKey }) {
   const decoratedOnClick = useAccordionButton(eventKey, () =>
-    console.log('totally custom!'),
   );
 
   return (
