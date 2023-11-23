16b2451bfb304000091518b6a8321eeda3a511c7
Shivangi Srivastava <shi06dec@gmail.com>
2023-11-22 17:33:09 -0700

    Work around pagination

--- frontend/src/components/RemindersTable.jsx
+++ frontend/src/components/RemindersTable.jsx
@@ -3,11 +3,56 @@ import UpcomingReminderItem from './UpcomingReminderItem';
 import CompletedReminderItem from './CompletedReminderItem';
 import ReminderAccordion from './ReminderAccordion';
 import { useState, useEffect } from 'react';
+import Pagination from 'react-bootstrap/Pagination';
 import axios from 'axios';
+
+let active = 2;
+let items = [];
+for (let number = 1; number <= 5; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === active}>
+      {number}
+    </Pagination.Item>,
+  );
+}
 
 function RemindersTable() {
   const [upcomingReminders, setUpcomingReminders] = useState([]);
   const [completedReminders, setCompletedReminders] = useState([]);
+  const [state, setState] = useState({
+    data: [],
+    limit: 10,
+    activePage: 1
+  });
+
+  useEffect(() => {
+    loadReminders();
+    axios
+      .get(
+        `http://localhost:8080/api/reminders/upcoming?_page=1&_limit=${state.limit}`
+      )
+      .then((res) => {
+        setState((prev) => ({
+          ...prev,
+          data: res.data
+        }));
+      })
+      .catch((error) => console.log(error));
+  }, [state.limit]);
+
+  const handlePageChange = (pageNumber) => {
+    setState((prev) => ({ ...prev, activePage: pageNumber }));
+
+    axios
+      .get(`http://localhost:8080/api/reminders/upcoming?_page=${pageNumber}`)
+      .then((res) => {
+        setState((prev) => ({
+          ...prev,
+          data: res.data
+        }));
+      })
+      .catch((error) => console.log(error));
+  };
 
   const loadReminders = () => {
     const upcomingRemindersPath = 'http://localhost:8080/api/reminders/upcoming';
@@ -28,9 +73,9 @@ function RemindersTable() {
       });
   };
 
-  useEffect(() => {
-    loadReminders();
-  }, []);
+  // useEffect(() => {
+  //   loadReminders();
+  // }, []);
 
   return (
     <div>
@@ -40,6 +85,29 @@ function RemindersTable() {
         }}
       />
       <h2 className='headers'>Upcoming Reminders</h2>
+      {/* <ul className="list-group p-4">
+        {state.data.map((item) => {
+          return (
+            <li key={item.id} className="list-group-item">
+              <span className="font-weight-bold pr-2">{item.id}.</span>{" "}
+              {item.title}
+            </li>
+          );
+        })}
+      </ul> */}
+      <Pagination className="px-4">
+        {state.data.map((_, index) => {
+          return (
+            <Pagination.Item
+              onClick={() => handlePageChange(index + 1)}
+              key={index + 1}
+              active={index + 1 === state.activePage}
+            >
+              {index + 1}
+            </Pagination.Item>
+          );
+        })}
+      </Pagination>
       <Table responsive='sm'>
         <thead>
           <tr>
