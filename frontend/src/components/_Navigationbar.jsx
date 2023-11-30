b62d1709e4030cd45659d2bb6174b3073cc04a41
kazvee <kayvee123@gmail.com>
2023-11-30 15:00:22 -0700

    Add About link to Navbar

--- frontend/src/components/_Navigationbar.jsx
+++ frontend/src/components/_Navigationbar.jsx
@@ -8,29 +8,60 @@ function Navigationbar(props) {
 
   const togglemodes = () => {
     const element = document.body;
-    element.dataset.bsTheme = element.dataset.bsTheme === "light" ? "dark" : "light";
-  }
+    element.dataset.bsTheme =
+      element.dataset.bsTheme === 'light' ? 'dark' : 'light';
+  };
 
   return (
     <>
-      <Navbar bg='dark' data-bs-theme='dark' fixed="top" className="nav-bar">
-        <Navbar.Brand className="logo" onClick={() => setShowPets()} aria-label="PawTrackr - Home">
-          <img src={logo} alt='pawtrackr logo with pawprint' className='main-logo'></img>
+      <Navbar bg='dark' data-bs-theme='dark' fixed='top' className='nav-bar'>
+        <Navbar.Brand
+          className='logo'
+          onClick={() => setShowPets()}
+          aria-label='PawTrackr - Home'
+        >
+          <img
+            src={logo}
+            alt='pawtrackr logo with pawprint'
+            className='main-logo'
+          ></img>
         </Navbar.Brand>
         <Nav className='me-auto'>
-          <Nav.Link onClick={() => setShowPets()} aria-label="Go to Pets">Pets</Nav.Link>
-          <Nav.Link onClick={() => setShowReminders()} aria-label="Go to Reminders">Reminders</Nav.Link>
-          <Nav.Link onClick={() => setShowContacts()} aria-label="Go to Contacts">Contacts</Nav.Link>
+          <Nav.Link onClick={() => setShowPets()} aria-label='Go to Pets'>
+            Pets
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => setShowReminders()}
+            aria-label='Go to Reminders'
+          >
+            Reminders
+          </Nav.Link>
+          <Nav.Link
+            onClick={() => setShowContacts()}
+            aria-label='Go to Contacts'
+          >
+            Contacts
+          </Nav.Link>
+          <Nav.Link>
+            <a
+              href='https://github.com/Devvaru/PawTrackr/'
+              target='_blank'
+              rel='noreferrer'
+              style={{ textDecoration: 'none', color: '#FFFFFF8C' }}
+            >
+              About
+            </a>
+          </Nav.Link>
         </Nav>
 
         <Form>
           <Form.Check
-            type="switch"
-            id="custom-switch"
-            label="Toggle Light/Dark Mode"
+            type='switch'
+            id='custom-switch'
+            label='Toggle Light/Dark Mode'
             onClick={togglemodes}
-            style={{ color: "rgba(255, 255, 255, 0.55)" }}
-            aria-label="Toggle Light/Dark Mode"
+            style={{ color: 'rgba(255, 255, 255, 0.55)' }}
+            aria-label='Toggle Light/Dark Mode'
           />
         </Form>
       </Navbar>
