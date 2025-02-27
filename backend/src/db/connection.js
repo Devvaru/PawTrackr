232c29b2c6d91de22f02911747aa16054f88f2e4
Devvaru <syeraru@gmail.com>
2025-02-26 17:22:58 -0700

    Change params to database url

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,15 +1,22 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const dbParams = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-};
+// const dbParams = {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// };
 
-const db = new Pool(dbParams);
+// const db = new Pool(dbParams);
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render PostgreSQL
+  },
+})
 
 db.connect();
 
